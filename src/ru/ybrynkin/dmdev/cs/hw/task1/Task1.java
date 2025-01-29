383765363f8082076c5aaac7a8ce58a164aab44f
YBrynkin <abrynkin@yandex.ru>
2025-01-29 17:08:41 +0300

    initial commit of the project

---
+++ src/ru/ybrynkin/dmdev/cs/hw/task1/Task1.java
@@ -0,0 +1,40 @@
+package ru.ybrynkin.dmdev.cs.hw.task1;
+
+/**
+ * Задание 1
+ *
+ * В переменной minutes лежит число от 0 до 59. Написать функцию, которая принимает в качестве
+ * параметра значение переменной minutes и выводит на консоль в какую четверть часа попадает это
+ * число (в первую, вторую, третью или четвертую).</p>
+ *
+ * Протестировать функцию в main.
+ */
+
+public class Task1 {
+
+  private static void printQuarterOfHour(int minutes) {
+    if (minutes > 0) {
+      int quarter = minutes / 15 + 1;
+      switch (quarter) {
+        case 1 -> System.out.println("1st quarter");
+        case 2 -> System.out.println("2nd quarter");
+        case 3 -> System.out.println("3d quarter");
+        case 4 -> System.out.println("4th quarter");
+        default -> System.out.println("Wrong input: the value exceeds 60!");
+      }
+    } else {
+      System.out.println("Wrong input: the value is below zero!");
+    }
+  }
+
+  public static void main(String[] args) {
+    //Test hour function
+    printQuarterOfHour(2); // 1st
+    printQuarterOfHour(17); // 2d
+    printQuarterOfHour(43); // 3d
+    printQuarterOfHour(59); // 4th
+    printQuarterOfHour(61); //Wrong
+    printQuarterOfHour(-10); //Wrong
+  }
+
+}
---
+++ src/ru/ybrynkin/dmdev/cs/hw/task1/Task3.java
@@ -0,0 +1,51 @@
+package ru.ybrynkin.dmdev.cs.hw.task1;
+
+/**
+ * Задание 3
+ * <p>
+ * Даны два прямоугольных треугольника. Каждый из них задается двумя целочисленными переменными a и
+ * b - стороны треугольника. Написать код, в котором вычисляется площадь каждого треугольника и
+ * затем эти площади сравниваются друг с другом.
+ * <p>
+ * Для этого понадобится написать 2 функции. Первая: по двум сторонам прямоугольного треугольника
+ * возвращает его площадь. Вторая: сравнивает переданные площади двух треугольников и выводит на
+ * консоль первый треугольник больше, меньше или равен второму. Учитывать, что площадь может быть
+ * вещественным числом.
+ */
+
+public class Task3 {
+
+  private static double getTriangleArea(int a, int b) {
+    return (double) (a * b) / 2;
+  }
+
+  private static void printTriangleComparingResult(double s1, double s2) {
+    if (s1 > s2) {
+      System.out.println("The 1st triangle with is bigger than the 2nd");
+    } else if (s1 < s2) {
+      System.out.println("The 1st triangle is smaller than the 2nd");
+    } else {
+      System.out.println("The 1st triangle is equal to the 2nd");
+    }
+  }
+
+  public static void main(String[] args) {
+    
+    int[][] legs1 = {{5, 5}, {4, 4}, {7, 9}, {12, 13}};
+    int[][] legs2 = {{4, 6}, {4, 4}, {6, 10}, {11, 14}};
+
+    System.out.println(getTriangleArea(7,8));
+
+    for (int i = 0; i < legs1.length; i++) {
+      double s1 = getTriangleArea(legs1[i][0], legs1[i][1]);
+      double s2 = getTriangleArea(legs2[i][0], legs2[i][1]);
+
+      System.out.printf("a: %d, b: %d, s1: %f \n", legs1[i][0], legs1[i][1], s1);
+      System.out.printf("a: %d, b: %d, s2: %f \n", legs2[i][0], legs2[i][1], s2);
+      printTriangleComparingResult(s1, s2);
+    }
+  
+  }
+}
+
+
